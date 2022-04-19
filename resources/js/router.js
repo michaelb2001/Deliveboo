ddd031e64290c30ebe5fa442476158bf58a453b5
michaelb2001 <brunco.michael@gmail.com>
2022-04-19 16:41:38 +0200

    tentativo installazione vue

---
+++ resources/js/router.js
@@ -0,0 +1,28 @@
+import Vue from "vue";
+        import VueRouter from "vue-router";
+
+        Vue.use(VueRouter);
+
+        import Home from "./pages/Home";
+        /*import SinglePost from "./pages/SinglePost";
+        import SearchedUser from "./pages/SearchedUser";
+        import SearchTag from "./pages/SearchTag";
+        import NotFound from "./pages/NotFound";*/
+
+        const router = new VueRouter({
+            mode: "history",
+            routes: [
+                {
+                    path: "/",
+                    name: "home",
+                    component: Home
+                },
+                {
+                    path: "*",
+                    name: "page-404",
+                    component: NotFound
+                },
+            ]
+        });
+
+        export default router
