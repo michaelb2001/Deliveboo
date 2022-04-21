fabf5f41400847f7fc5b6038d698cdb86f69e606
ValleLuca <valle.lucaAb@outlook.it>
2022-04-21 16:30:52 +0200

    Focus card

--- resources/js/common/FocusCard.vue
+++ resources/js/common/FocusCard.vue
@@ -1,7 +1,31 @@
 <template>
-  <div class="card-container">
-      <div  class="focus-card">
-        {{plate.name}}
+  <div class="position-relative w-100">
+      <div class="focus-cards">
+
+        <div class="container">
+          <div class="row flex-column">
+            <div class="col p-0">
+              
+              <div>
+                <img class="img-fluid img-class" v-if="plate.img" :src="`../storage/${plate.img}`">
+                <img class="img-fluid img-class" v-else src="https://consumer-component-library.roocdn.com/25.27.6/static/images/placeholder.svg">
+              </div>
+
+            </div>
+            <div class="col">
+               <div>
+                <h4>{{plate.name}}</h4>
+              </div>
+              <div>
+                <p>{{plate.ingredients}}</p>
+              </div>
+            </div>
+            <div class="col">
+              <button type="button" class="btn btn-primary">{{plate.price}}€</button>
+            </div>
+          </div>
+        </div>
+        
       </div>
   </div>
 </template>
@@ -16,12 +40,21 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.focus-card{
-    position: absolute;
-    bottom:100px;
-    left:100px;
-    background: red;
-    height: 200px;
-    width: 200x;
+
+.focus-cards{
+  position: fixed;
+  top: 500px;
+  left: 500px;
+  background-color: red;
+  height: 600px;
+  width: 560px;
+
+  .img-class{
+    width: 560px;
+    height: 315px;
+    object-fit: cover;
+  }
+
 }
+
 </style>
