7006a4416df4f778f53e9b1cbe8dab42815f346e
alessandro-cappelli <alessandrocappelli0507@gmail.com>
2022-04-21 16:01:19 +0200

    plate-blur

--- resources/js/common/CardPlate.vue
+++ resources/js/common/CardPlate.vue
@@ -1,11 +1,9 @@
 <template>
   <div class="card_container">
-      <div>
-        <div class="col">
-          <p class="ms_name_plate">{{plate.name}}</p>
-          <p class="ms_ingredients">{{plate.ingredients}}</p>
-          <p>{{plate.price}}€</p>
-        </div>
+      <div class="col">
+        <p class="ms_name_plate">{{plate.name}}</p>
+        <p class="ms_ingredients">{{plate.ingredients}}</p>
+        <p>{{plate.price}}€</p>
       </div>
   </div>
 </template>
@@ -27,6 +25,15 @@ export default {
 
 .ms_ingredients{
   margin-bottom: 0;
+}
+
+.col{
+  padding: 10px;
+  background: rgba( 255, 255, 255, 0.9 );
+  box-shadow: 0 8px 32px 0 rgba( 31, 38, 135, 0.37 );
+  backdrop-filter: blur( 20px );
+  -webkit-backdrop-filter: blur( 20px );
+  border-radius: 10px;
 }
 
 </style>
