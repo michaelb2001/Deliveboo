4d5915d56892522d902fcbbd47a756e79b98dd23
nadiaf85 <nadia.fiorio@alice.it>
2022-04-23 18:48:17 +0200

    order plate style

--- resources/views/admin/order.blade.php
+++ resources/views/admin/order.blade.php
@@ -2,12 +2,21 @@
 @extends('layouts.navbar_admin')
 
 @section('content')
+<div class="d-flex flex-row flex-wrap justify-content-around">
     @foreach ($orders as $order)
-    <h2>Prenotazione da {{$order->name}} {{$order->surname}}</h2>
-        @foreach ($order->plates as $plate)
-            <p>{{$plate->name}}</p>
-        @endforeach
-        <h2>All'indirizzo da {{$order->address}}</h2>
-        <h2>telefono {{$order->phone}}</h2>
-    @endforeach
+    <div class="box-order">
+        <span class="order-from">Prenotazione da: <br> <h4>{{$order->name}} {{$order->surname}}</h4></span>
+            <h3>Piatto: </h3>
+            <div class="order-plate">
+                @foreach ($order->plates as $plate)
+                    <span class="plate"><h4>{{$plate->name}}</h4></span>
+                @endforeach
+            </div>
+            <span class="order-address">All'indirizzo: <br>  <h4>{{$order->address}}</h4></span>
+            <span class="phone">Numero di telefono: <br> <h4>{{$order->phone}}</h4></span>
+    </div>
+@endforeach
+
+</div>
+
 @endsection
