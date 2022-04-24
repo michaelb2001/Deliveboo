2f0b03369d06f66186980c06b6dc0d51b7638ec0
nadiaf85 <nadia.fiorio@alice.it>
2022-04-24 13:01:44 +0200

    page 404

--- resources/js/pages/NotFound.vue
+++ resources/js/pages/NotFound.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="container">
-      page 404
+    <div class="not-found">
+      <div class="image-error"></div>
+      <div class="text">
+        <h1>Pagina Non Trovata</h1>
+        <p>La pagina che stai cercando non esiste</p>
+        <router-link class="back" :to="{ name: 'main'}">Torna alla Home </router-link>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -10,6 +17,49 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
 
+.not-found{
+    background: url('https://deliveroo.it/assets/images/error-pages/background-7edaa013aceff22965bdb7fbf274b4a2.svg');
+    text-align: center;
+    padding: 137px 16px 32px;
+    position: relative;
+    height: 300px;
+
+      h1{
+        font-size: 40px;
+      }
+}
+
+.image-error{
+    background: url('https://deliveroo.it/assets/images/error-pages/roo-5c784b3ea96399e6269af8a11ec6349c.svg');
+    position: absolute;
+    width: 200px;
+    height: 145px;
+    left: 450px;
+    top: 200px;
+}
+
+.text{
+    position: absolute;
+    top: 390px;
+    left: 390px;
+    text-align: center;
+
+  p{
+    margin-bottom: 50px;
+  }
+
+  .back{
+    background-color: #00ccbb;
+    color: white;
+    font-size: 15px;
+    padding: 9px;
+    border-radius: 10px;
+
+    &:hover{
+      text-decoration: none;
+    }
+  }
+}
 </style>
