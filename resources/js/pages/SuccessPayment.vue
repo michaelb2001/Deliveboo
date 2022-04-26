b05221db60e068bc7f93182f3fdd3d8b96257a01
ValleLuca <valle.lucaAb@outlook.it>
2022-04-26 17:06:26 +0200

    Style pagamento completato

--- resources/js/pages/SuccessPayment.vue
+++ resources/js/pages/SuccessPayment.vue
@@ -1,26 +1,31 @@
 <template>
-  <div v-if="show" class="main-container">
-     <!-- <h1> {{user.activity}} </h1> -->
-     <h1> nome attività </h1>
-      Grazie Di Aver Scelto Noi ! 
-      <h3>Il tuo ordine : </h3>
-      <!-- <ul v-if="cart">  il v-if VA TOLTO ALLA FINE DI LUCA 
-          <li v-for="(elem,index) in cart" :key="index">
-              {{elem.plate.name}} - {{elem.quantity}}x
-          </li>
-      </ul> -->
-      <ul>
-          <li v-for="(elem,index) in 3" :key="index">
-              piatto{{index}} - {{index + 1}}x
-          </li>
-      </ul>
+  <div v-if="show" class="main-container d-flex align-items-center flex-column">
+    <div class="back text-center">
+        <!-- <h1> {{user.activity}} </h1> -->
+        <div>
+            <h1> Nome attività </h1>
+            <p>Grazie per averci scelto !</p> 
+            <h3>Il tuo ordine :</h3>            
+        </div>
+        <!-- <ul v-if="cart">  il v-if VA TOLTO ALLA FINE DI LUCA 
+            <li v-for="(elem,index) in cart" :key="index">
+                {{elem.plate.name}} - {{elem.quantity}}x
+            </li>
+        </ul> -->
+        <ul>
+            <li v-for="(elem,index) in 3" :key="index">
+                piatto{{index}} - {{index + 1}}x
+            </li>
+        </ul>
 
-    <!-- <router-link class="back" :to="{ name: 'main'}">
-        Torna alla Home 
-    </router-link> -->
-    <a href="/">
-        Torna alla Home
-    </a>
+        <!-- <router-link class="back" :to="{ name: 'main'}">
+    Torna alla Home 
+</router-link> -->
+        <a href="/">
+            <button type="button" class="btn option-btn">Torna alla home</button>
+        </a>
+    </div>
+        
   </div>
 </template>
 
@@ -49,15 +54,34 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import '../../sass/_variables.scss';
+
+
+ul{
+    list-style-type: none;
+    padding: 0px;
+}
+
 .back{
     background-color: #00ccbb;
     color: white;
     font-size: 15px;
     padding: 9px;
     border-radius: 10px;
-
-    &:hover{
-      text-decoration: none;
+    width: 80%;
+    a{
+        text-decoration-line: none;
     }
   }
+
+.option-btn{
+    color: $light-color;
+    background-color: $primary-color;
+    border: 1px solid $third-color;
+    &:hover{
+    color: $primary-color;
+    background-color: $third-color;
+    border: 1px solid $third-color;
+    }
+}
 </style>
