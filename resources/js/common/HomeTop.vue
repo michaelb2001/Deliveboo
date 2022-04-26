b3989dd9acd21a96958fb687e92797b4a43e07d9
antoniom28 <antomarc@gmail.com>
2022-04-26 22:33:40 +0200

    homepage

---
+++ resources/js/common/HomeTop.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="home-top">
+    <input v-model="inputText" type="text" placeholder="indirizzo">
+    <div v-if="inputText && inputText.length > 0">
+        <button> 
+            <a :href="`/main/${inputText}`">Cerca</a>
+        </button>
+    </div>
+
+    <div v-else>
+        <button disabled> 
+            Cerca
+        </button>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+    name:"HomeTop",
+    data(){
+        return{
+            inputText: "",
+        }
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- resources/js/pages/Home.vue
+++ resources/js/pages/Home.vue
@@ -1,142 +1,19 @@
 <template>
-  <div>
-    <input v-model="inputText" type="text" placeholder="indirizzo">
-    <div v-if="inputText && inputText.length > 0">
-        <button> 
-            <a :href="`/main/${inputText}`">Cerca</a>
-        </button>
-    </div>
-
-    <div v-else>
-        <button disabled> 
-            Cerca
-        </button>
-    </div>
-
-    <h1>La selezione di Deliveboo</h1>
-    <div class="selection">
-        <div class="container-info">
-            <div class="comfort-food">
-                <h1>Comfort food</h1>
-            </div>
-            <div class="text-info">
-                <p>I nostri classici che scaldano il cuore, perfetti in ogni momento.</p>
-                <p><a href="">Scopri Comfort food</a></p>
-            </div>
-        </div>
-        <div class="container-info">
-            <div class="dessert">
-                <h1>Dolci e dessert</h1>
-            </div>
-            <div class="text-info">
-                <p>Dolci piaceri per rendere la giornata ancora più gustosa.</p>
-                <p><a href="">Scopri Dolci e dessert</a></p>
-            </div>
-        </div>
-        <div class="container-info">
-            <div class="share">
-                <h1>Perfetti da condividere</h1>
-            </div>
-            <div class="text-info">
-                <p>Serve una scusa per stare insieme? Ordina dai ristoranti che trasformeranno la tua serata in una vera festa</p>
-                <p><a href="">Scopri Perfetti da condividere</a></p>
-            </div>
-        </div>
-        <div class="container-info">
-            <div class="exclusive">
-                <h1>Esclusiva Deliveboo</h1>
-            </div>
-            <div class="text-info">
-                <p>I più famosi, i più buoni, i preferiti. Quelli che trovi solo su Deliveboo.</p>
-                <p><a href="">Scopri Esclusiva Deliveboo</a></p>
-            </div>
-        </div>
-    </div> 
+<div class="home">
+    <HomeTop />
+    <HomeDown />
 </div>
-        
 </template>
 
 <script>
+import HomeDown from '../common/HomeDown.vue'
+import HomeTop from '../common/HomeTop.vue'
 export default {
     name:"Home",
-    data(){
-        return{
-            inputText: "",
-        }
-    },
+    components: { HomeTop, HomeDown },
 }
 </script>
 
 <style lang="scss" scoped>
 
-.selection{
-    display: flex;
-    flex-wrap: wrap;
-
-    .comfort-food{
-        width: 100%;
-        height: 150px;
-        background: url('https://f.roocdn.com/images/menu_tags/288/menu-tag-image.jpg?width=430&height=150&auto=webp&format=jpg&fit=crop&v=1648802301');
-        background-repeat: no-repeat;
-        background-size: cover;
-    }
-    .dessert{
-        min-width: 700px;
-        height: 150px;
-        background: url('https://f.roocdn.com/images/menu_tags/328/menu-tag-image.jpg?width=660&height=150&auto=webp&format=jpg&fit=crop&v=1548761261');
-        background-repeat: no-repeat;
-        background-size: cover;
-    }
-    .share{
-        min-width: 100%;
-        height: 150px;
-        background: url('https://f.roocdn.com/images/menu_tags/291/menu-tag-image.jpg?width=660&height=150&auto=webp&format=jpg&fit=crop&v=1547125828');
-        background-repeat: no-repeat;
-        background-size: cover;
-    }
-    .exclusive{
-        width: 100%;
-        height: 150px;
-        background: url('https://f.roocdn.com/images/menu_tags/293/menu-tag-image.jpg?width=430&height=150&auto=webp&format=jpg&fit=crop&v=1547125827');
-        background-repeat: no-repeat;
-        background-size: cover;
-    }
-
-    .comfort-food, .dessert, .share, .exclusive h1{
-        color: white;
-    }
-
-    .comfort-food, .dessert, .share, .exclusive{
-        display: flex;
-        justify-content: center;
-        align-items: center;
-
-        h1{
-            font-family: sans-serif;
-            font-weight: bold;
-        }
-    }
-
-    .text-info a{
-        color: #00B8A9;
-        text-decoration: none;
-    }
-
-    .text-info p{
-        color: black;
-    }
-
-}
-
-.container-info{
-    display: flex;
-    flex-direction: column;
-    margin-right: 20px;
-}
-
-
-
-
-
-/*transform: skewY(-6deg);*/
 </style>
